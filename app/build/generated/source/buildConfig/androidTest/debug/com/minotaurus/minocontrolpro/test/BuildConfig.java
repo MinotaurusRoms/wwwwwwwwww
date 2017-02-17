52cab592efc66352de3d658e123b4f6656c18be6
asc1977 <schwenn1977@gmail.com>
2017-02-17 16:50:02 +0100

    V4.0

--- app/build/generated/source/buildConfig/androidTest/debug/com/minotaurus/minocontrolpro/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/minotaurus/minocontrolpro/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.minotaurus.minocontrolpro.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 23;
-  public static final String VERSION_NAME = "23.0";
+  public static final int VERSION_CODE = 36;
+  public static final String VERSION_NAME = "36.0";
 }
