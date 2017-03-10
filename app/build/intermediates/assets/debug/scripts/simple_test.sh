1fd4ed0198347f7f338ac32ec91b50b018f7ddbb
asc1977 <schwenn1977@gmail.com>
2017-03-10 11:40:43 +0100

    V4.4

--- app/build/intermediates/assets/debug/scripts/simple_test.sh
+++ app/build/intermediates/assets/debug/scripts/simple_test.sh
@@ -1,3 +1,8 @@
 #!/system/bin/sh
-su
-busybox echo "test"
+
+if [ -f /system/csc/others.xml ] ; then
+   sed -i 's/<CscFeature_SystemUI_ConfigMaxRssiLevel>4/<CscFeature_SystemUI_ConfigMaxRssiLevel>5/g' /system/csc/others.xml
+    chown 0:0 /system/csc/others.xml
+    chmod 644 /system/csc/others.xml
+    chcon u:object_r:system_file:s0 /system/csc/others.xml
+fi
